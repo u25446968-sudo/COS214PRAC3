0367857ad57f4b69ced72e4777f053cd95c8bdb6
RecursiveAsh <boitu.ashley13@gmail.com>
2026-08-28 23:13:20 +0200

    Added ShuttleStop.h

---
+++ ShuttleStop.h
@@ -0,0 +1,16 @@
+#ifndef SHUTTLESTOP_H
+#define SHUTTLESTOP_H
+
+#include "Observer.h"
+#include "EventUnit.h"
+
+enum class TransportStatus {RUNNING, HALTED, DELAYED};
+
+class SecurityTeam : public EventUnit, public Observer{
+    private:
+       TransportStatus transportStatus;
+    public: 
+       void update(const Notice& notice)override;
+};
+
+#endif
